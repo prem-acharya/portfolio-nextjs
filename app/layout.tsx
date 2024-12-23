f3b22d1a81e2b66c429c27e480a47b12f15a4c11
Prem Acharya <102874190+prem-acharya@users.noreply.github.com>
2024-12-23 16:47:47 +0530

    Updated portfolio metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,54 @@ import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+interface ExtendedMetadata extends Metadata {
+  openGraph?: {
+    title: string;
+    description: string;
+    url: string;
+    site_name: string;
+    images: {
+      url: string;
+      width: number;
+      height: number;
+      alt: string;
+    }[];
+  };
+  twitter?: {
+    card: string;
+    title: string;
+    description: string;
+    image: string;
+  };
+}
+
+export const metadata: ExtendedMetadata = {
   icons: {
     icon: '/favicon.ico',
   },
-  title: 'Prem Acharya | Full-Stack Developer & React, Next.js Specialist',
-  description: 'Discover Prem Acharya\'s portfolio featuring innovative web development, React and Next.js expertise, and scalable full-stack solutions for modern businesses.',
+  title: 'Prem Acharya | Full-Stack Developer & React, Next.js Enthusiast',
+  description: 'Discover Prem Acharya\'s portfolio featuring innovative web development, enthusiasm for React.js and Next.js, and scalable full-stack solutions for modern businesses.',
   keywords: 'Prem Acharya, full-stack developer, React developer, Next.js developer, web development portfolio, modern web design, freelance web developer, API integration, scalable web solutions',
+  openGraph: {
+    title: 'Prem Acharya | Full-Stack Developer & React, Next.js Enthusiast',
+    description: 'Discover Prem Acharya\'s portfolio featuring innovative web development, enthusiasm for React.js and Next.js, and scalable full-stack solutions for modern businesses.',
+    url: 'https://premacharya.vercel.app/',
+    site_name: 'Prem Acharya Portfolio',
+    images: [
+      {
+        url: '/public/site_img.webp',
+        width: 800,
+        height: 600,
+        alt: 'Prem Acharya Portfolio Image',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Prem Acharya | Full-Stack Developer & React, Next.js Enthusiast',
+    description: 'Discover Prem Acharya\'s portfolio featuring innovative web development, enthusiasm for React.js and Next.js, and scalable full-stack solutions for modern businesses.',
+    image: '/public/site_img.webp',
+  },
 };
 
 export default function RootLayout({
