3578a89a4703a6b812c76fc56e678b6c020b2b26
Prem Acharya <102874190+prem-acharya@users.noreply.github.com>
2024-12-23 17:38:44 +0530

    Fixed image in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,58 +4,18 @@ import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Navbar } from '@/components/navbar';
 import { Footer } from '@/components/footer';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
-interface ExtendedMetadata extends Metadata {
-  openGraph?: {
-    title: string;
-    description: string;
-    url: string;
-    site_name: string;
-    images: {
-      url: string;
-      width: number;
-      height: number;
-      alt: string;
-    }[];
-  };
-  twitter?: {
-    card: string;
-    title: string;
-    description: string;
-    image: string;
-  };
-}
-
-export const metadata: ExtendedMetadata = {
+export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
   },
   title: 'Prem Acharya | Full-Stack Developer & React, Next.js Enthusiast',
   description: 'Discover Prem Acharya\'s portfolio featuring innovative web development, enthusiasm for React.js and Next.js, and scalable full-stack solutions for modern businesses.',
   keywords: 'Prem Acharya, full-stack developer, React developer, Next.js developer, web development portfolio, modern web design, freelance web developer, API integration, scalable web solutions',
-  openGraph: {
-    title: 'Prem Acharya | Full-Stack Developer & React, Next.js Enthusiast',
-    description: 'Discover Prem Acharya\'s portfolio featuring innovative web development, enthusiasm for React.js and Next.js, and scalable full-stack solutions for modern businesses.',
-    url: 'https://premacharya.vercel.app/',
-    site_name: 'Prem Acharya Portfolio',
-    images: [
-      {
-        url: '/public/site_img.webp',
-        width: 800,
-        height: 600,
-        alt: 'Prem Acharya Portfolio Image',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Prem Acharya | Full-Stack Developer & React, Next.js Enthusiast',
-    description: 'Discover Prem Acharya\'s portfolio featuring innovative web development, enthusiasm for React.js and Next.js, and scalable full-stack solutions for modern businesses.',
-    image: '/public/site_img.webp',
-  },
 };
 
 export default function RootLayout({
@@ -65,6 +25,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta property="og:title" content="Prem Acharya | Full-Stack Developer & React, Next.js Enthusiast" />
+        <meta property="og:description" content="Discover Prem Acharya's portfolio featuring innovative web development, enthusiasm for React.js and Next.js, and scalable full-stack solutions for modern businesses." />
+        <meta property="og:url" content="https://premacharya.vercel.app/" />
+        <meta property="og:site_name" content="Prem Acharya Portfolio" />
+        <meta property="og:image" content="/site_img.webp" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Prem Acharya | Full-Stack Developer & React, Next.js Enthusiast" />
+        <meta name="twitter:description" content="Discover Prem Acharya's portfolio featuring innovative web development, enthusiasm for React.js and Next.js, and scalable full-stack solutions for modern businesses." />
+        <meta name="twitter:image" content="/site_img.webp" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
