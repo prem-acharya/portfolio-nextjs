bb46db9112463daa6b856779c79bf984580b8e78
Prem Acharya <102874190+prem-acharya@users.noreply.github.com>
2025-01-24 13:27:48 +0530

    Added employmentType, locationType & skills in experience timeline components

--- components/sections/experience/timeline-item.tsx
+++ components/sections/experience/timeline-item.tsx
@@ -10,19 +10,28 @@ export function TimelineItem() {
       title: "Full Stack Developer",
       company: "Credsoft Technologies",
       date: "Sep 2024 - Present",
+      employmentType: "Full Time",
+      locationType: "On-site",
       description: "At Credsoft Technologies, I work as a Full Stack Developer, building functional and user-friendly web applications using tools like React, Next, Node and many more.",
+      skills: ["MERN Stack", "Next JS", "Node JS"],
     },
     {
       title: "React Developer",
       company: "Credsoft Technologies",
-      date: "Mar 2024 - Aug 2024 • 5 months",
+      date: "Mar 2024 - Aug 2024 • 6 mos",
+      employmentType: "Internship",
+      locationType: "On-site",
       description: "As a React Developer, I enhanced my skills in React, explored Node.js and Express, worked with MongoDB, and gained experience in API development and integration.",
+      skills: ["React JS", "Node JS", "Express JS", "MongoDB"],
     },
     {
       title: "Web Developer",
       company: "InfiniteAI Technology Private Limited",
-      date: "May 2023 - Jun 2023 • 2 months",
+      date: "May 2023 - Jun 2023 • 2 mos",
+      employmentType: "Internship",
+      locationType: "On-site",
       description: "I completed an internship at InfiniteAI Technology Private Limited, where I worked with React.js and Tailwind CSS. During this time, I gained hands-on experience and learned the fundamentals of React JS.",
+      skills: ["React JS", "Tailwind CSS"],
     },
   ];
   
@@ -31,9 +40,18 @@ export function TimelineItem() {
       title: experiences[0].title,
       content: (
         <div className="space-y-2">
-          <span className="text-sm text-foreground">{experiences[0].date}</span>
+          <span className="text-sm text-foreground font-medium">{experiences[0].date}</span>
           <h3 className="text-lg text-blue-500 dark:text-blue-400 font-medium ">{experiences[0].company}</h3>
-          <p className="text-muted-foreground">{experiences[0].description}</p>
+          <span className="text-sm text-foreground font-medium">{experiences[0].employmentType}{' • '}{experiences[0].locationType}</span>
+          
+          <div className="flex flex-col gap-2">
+            <p className="text-muted-foreground">{experiences[0].description}</p>
+            <div className="flex flex-wrap gap-2">
+              {experiences[0].skills.map((skill) => (
+                <span key={skill} className="text-sm text-foreground bg-foreground/10 px-3 py-1 rounded-full">{skill}</span>
+              ))}
+            </div>
+          </div>
         </div>
       ),
     },
@@ -41,9 +59,15 @@ export function TimelineItem() {
       title: experiences[1].title,
       content: (
         <div className="space-y-2">
-          <span className="text-sm text-foreground">{experiences[1].date}</span>
+          <span className="text-sm text-foreground font-medium">{experiences[1].date}</span>
           <h3 className="text-lg text-blue-500 dark:text-blue-400 font-medium ">{experiences[1].company}</h3>
+          <span className="text-sm text-foreground font-medium">{experiences[1].employmentType}{' • '}{experiences[1].locationType}</span>
           <p className="text-muted-foreground">{experiences[1].description}</p>
+          <div className="flex flex-wrap gap-2">
+            {experiences[1].skills.map((skill) => (
+              <span key={skill} className="text-sm text-foreground bg-foreground/10 px-3 py-1 rounded-full">{skill}</span>
+            ))}
+          </div>
         </div>
       ),
     },
@@ -51,9 +75,15 @@ export function TimelineItem() {
       title: experiences[2].title,
       content: (
         <div className="space-y-2">
-          <span className="text-sm text-foreground">{experiences[2].date}</span>
+          <span className="text-sm text-foreground font-medium">{experiences[2].date}</span>
           <h3 className="text-lg text-blue-500 dark:text-blue-400 font-medium ">{experiences[2].company}</h3>
+          <span className="text-sm text-foreground font-medium">{experiences[2].employmentType}{' • '}{experiences[2].locationType}</span>
           <p className="text-muted-foreground">{experiences[2].description}</p>
+          <div className="flex flex-wrap gap-2">
+            {experiences[2].skills.map((skill) => (
+              <span key={skill} className="text-sm text-foreground bg-foreground/10 px-3 py-1 rounded-full">{skill}</span>
+            ))}
+          </div>
         </div>
       ),
     },
